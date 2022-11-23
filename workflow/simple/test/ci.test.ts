11ae27f9a38e1ed05380f1713998f8b3dbdc9042
Shubh Bapna <sbapna@redhat.com>
2022-11-23 15:07:15 -0500

    added simple workflow example

---
+++ workflow/simple/test/ci.test.ts
@@ -0,0 +1,37 @@
+import { MockGithub } from "@kie/mock-github";
+import { Act } from "@kie/act-js";
+import path from "path";
+
+let mockGithub: MockGithub;
+
+beforeEach(async () => {
+  // create a local repo
+  mockGithub = new MockGithub({
+    repo: {
+      testRepo: {
+        files: [
+          {
+            src: path.resolve(__dirname, "..", ".github"),
+            dest: ".github",
+          },
+        ],
+      },
+    },
+  });
+
+  await mockGithub.setup();
+});
+
+afterEach(async () => {
+  await mockGithub.teardown();
+});
+
+test("test workflow", async () => {
+  const act = new Act(mockGithub.repo.getPath("testRepo"));
+  const result = await act.runEvent("pull_request");
+
+  expect(result).toStrictEqual([
+    { name: "Main actions/checkout@v2", status: 0, output: "" },
+    { name: "Main do something", status: 0, output: "step 1\nstep 2" },
+  ]);
+});
